551402907d9ed8be85de03b8763173e0a175c5fb
Brandon Ma <56646867+ma-brandonjh@users.noreply.github.com>
2026-01-12 14:54:08 -0500

    made the label updated and added operation buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,67 @@
 "use client"
 
-import { useState, ChangeEvent } from "react";
+import { useState } from "react";
 
 export default function Home() {
 
-  const [result, setResult] = useState("")
+  const [result, setResult] = useState("0")
 
   const button_number: string[][] = [["+/-", "0" , "."], ["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+  const button_ops: string[][] = [["C", "DEL"], ["*", "/"], ["+", "-"]]
 
   const button_action = (button: string): void =>{
+    console.log(button)
     if (button === "+/-"){
-      
+      if(Number(result) > 0){
+        setResult("-" + result)
+      } else {
+        let pos_num = result.replace("-","")
+        setResult(pos_num)
+      }
     } else {
-      setResult(result + button)
+      if (result === "0"){
+        setResult(button)
+      } else {
+        setResult(result + button)
+      }
     }
   }
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-evenly py-32 px-8 bg-white dark:bg-black">
+
         <div className="bg-white">
           <input className="text-black text-right" type="text" value={result} defaultValue={0} readOnly/>
         </div>
-        <div className="grid grid-cols-3 gap-6 text-center">
-          {
-            button_number.reverse().map((button: string[]) =>
-              (
-                button.map((number: string) => (
-                  <button className="bg-white text-black w-auto" key={number} onClick={() => button_action(number)}>
-                    {number}
-                  </button>
-                ))
+        <div className="grid grid-cols-5 gap-6 text-center">
+          <div className="col-span-3 grid grid-cols-3 gap-6 text-center">
+            {
+              button_number.reverse().map((button: string[]) =>
+                (
+                  button.map((number: string) => (
+                    <button className="bg-white text-black w-16 h-16" key={number} onClick={() => button_action(number)}>
+                      {number}
+                    </button>
+                  ))
+                )
               )
-            )
-          }
+            }
+          </div>
+          <div className="col-span-2 grid grid-cols-2 gap-6 text-center">
+            {
+              button_ops.map((ops: string[]) => (
+                ops.map((op: string) => (
+                  <button className="bg-white text-black w-16 h-16" key={op} onClick={() => button_action(op)}>
+                      {op}
+                </button>
+                ))
+              ))
+            }
+            <button className=" bg-white text-black h-16 col-span-2 text-center">
+              =
+            </button>
+          </div>
         </div>
       </main>
     </div>
