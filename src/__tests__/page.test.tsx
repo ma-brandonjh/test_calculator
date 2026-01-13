c7a9bb0e70a9a97daaabec3a644a4de021b5a40e
Brandon Ma <56646867+ma-brandonjh@users.noreply.github.com>
2026-01-12 22:32:07 -0500

    removed unused import

--- src/__tests__/page.test.tsx
+++ src/__tests__/page.test.tsx
@@ -1,7 +1,6 @@
 import { expect, test } from "vitest"
 
 import { render, screen } from '@testing-library/react'
-import React from "react"
 import Page from "../app/page"
 
 test('addition', () => {
